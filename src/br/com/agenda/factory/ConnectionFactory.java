463792b82cdc52dcc926c2332aa6edb3c1403842
Caio Dutra Santos <93456721+caio-dutra@users.noreply.github.com>
2022-05-29 21:19:44 -0300

    Database connection/Conexão banco de dados

---
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,44 @@
+package br.com.agenda.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+	
+	// nome do usuário do mysql
+	private static final String USERNAME = "caio";
+	
+	// senha do banco 
+	private static final String PASSWORD = "12345";
+	
+	// caminho do banco de dados, porta, nome do banco de dados
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/java";
+	
+	/*
+	 * Conexão com banco de dados
+	 */
+	public static Connection createConnectionToMysql() throws Exception {
+		// Faz com que a classe seja carregada pela JVM
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// Cria a conexão com o banco de dados
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+		
+		return connection;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		// Recuperar uma conexão com o banco de dados
+		Connection con = createConnectionToMysql();
+		
+		// Testar se a conexão é nula
+		if(con!=null) {
+			System.out.println("Conexão obtida com sucesso!");
+			con.close();
+		}
+		
+	}
+	
+}
